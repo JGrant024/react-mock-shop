fe830cd8ac2eeb905d293a8acb2fae9180d590d6
JGrant024 <jonathan.grant23@icloud.com>
2024-02-22 12:12:22 -0500

    added product module for css styling

--- src/componets/ProductDetail.jsx
+++ src/componets/ProductDetail.jsx
@@ -1,7 +1,11 @@
+import styles from "./productDetails.module.csss";
+
 const ProductDetail = ({ product }) => {
   return (
-    <div>
+    <div className={styles.root}>
       <h2>{product.node.title}</h2>
+      <p>I AM PRODUCT DETAIL COMPONENT</p>
+      <div className="card-image"></div>
       <figure style={{ maxWidth: "200px" }}>
         <img
           src={product.node.featuredImage.url}
